3590ecad99b4e7966e41729f658dd340f99d620d
laychenyo33 <laychenyo33@gmail.com>
2019-04-03 16:20:57 +0800

    模組化

---
+++ resources/views/site/layout/ju_layout.blade.php
@@ -0,0 +1,32 @@
+<!-- layout.blade.php -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel 5 Example - @yield('title')</title>
+        <meta charset="utf-8">
+ 
+        @section('style')
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+        @show
+ 
+    </head>
+    <body>
+        <header>
+        </header>
+ 
+        <nav>
+        </nav>
+ 
+        <section>
+            @yield('content')
+        </section>
+ 
+        <footer>
+        </footer>
+ 
+        @section('script')
+        <script src="{{ asset('js/app.js') }}" ></script>
+        @show
+ 
+    </body>
+</html>
